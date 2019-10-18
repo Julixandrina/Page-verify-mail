e85b594e8b5fafe989fd324769ca7830c5b5d32a
Julixandrina <julixandrina@gmail.com>
2019-10-18 19:05:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Verify your mail</title>
+    <script> </script>
+</head>
+<body>
+<div class="main">
+    <div class="grid-container">
+        <div class="left"></div>
+        <div class="right"></div>
+    </div>
+    <div class="container">
+        <div class="content">
+            <a class="to-back" href="#">
+                <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M23.5 9.5H1.5M1.5 9.5L9.5 1.5M1.5 9.5L9.5 18.5" stroke="black" stroke-width="2"/>
+                </svg>
+            </a>
+            <div class="main-content">
+                <div class="title">Verify your email</div>
+                <div class="to-action">You will need to verify your email to complete registration</div>
+                <img src="/img/mailbox.svg" alt="">
+                <div class="info">An email has been to zexjulizrizh544@gmail.com with a link to verify your account.
+                    If you have not received the email after a few minutes, please check your spam folder</div>
+                <div class="buttons">
+                    <button type="button" class="resend button">Resend email</button>
+                    <a href="#" class="support button">Contact support</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</body>
+</html>
