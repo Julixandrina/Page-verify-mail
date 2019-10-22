31ebc293521d55d6ced7f58ba8117a4b820ac809
Julix <julixandrina@gmail.com>
2019-10-22 17:35:33 +0300

    Адаптивность и перестройка стрелки

--- index.php
+++ index.php
@@ -11,32 +11,36 @@
     <script> </script>
 </head>
 <body>
-<div class="main">
-    <div class="grid-container">
-        <div class="left"></div>
-        <div class="right"></div>
-    </div>
-    <div class="container">
-        <div class="content">
-            <a class="to-back" href="#">
-                <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M23.5 9.5H1.5M1.5 9.5L9.5 1.5M1.5 9.5L9.5 18.5" stroke="black" stroke-width="2"/>
-                </svg>
-            </a>
-            <div class="main-content">
-                <div class="title">Verify your email</div>
-                <div class="to-action">You will need to verify your email to complete registration</div>
-                <img src="/img/mailbox.svg" alt="">
-                <div class="info">An email has been to zexjulizrizh544@gmail.com with a link to verify your account.
-                    If you have not received the email after a few minutes, please check your spam folder</div>
-                <div class="buttons">
-                    <button type="button" class="resend button">Resend Email</button>
-                    <a href="#" class="support button">Contact Support</a>
-                </div>
-            </div>
+
+    <div class="page">
+        <div class="grid-container">
+            <div class="left"></div>
+            <div class="right"></div>
+
 
         </div>
+        <div class="container">
+            <div class="content">
+                <a class="to-back" href="#">
+                    <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M23.5 9.5H1.5M1.5 9.5L9.5 1.5M1.5 9.5L9.5 18.5" stroke="black" stroke-width="2"/>
+                    </svg>
+                </a>
+                <div class="main-content">
+                    <div class="title">Verify your email</div>
+                    <div class="to-action">You will need to verify your email to complete registration</div>
+                    <img src="/img/mailbox.svg" alt="">
+                    <div class="info">An email has been to zexjulizrizh544@gmail.com with a link to verify your account.
+                        If you have not received the email after a few minutes, please check your spam folder</div>
+                    <div class="buttons">
+                        <button type="button" class="resend button">Resend Email</button>
+                        <a href="#" class="support button">Contact Support</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-</div>
+
 </body>
 </html>
