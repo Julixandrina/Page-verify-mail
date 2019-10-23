c1f47e00d5e5536c87db41c1b11c266bd443d943
Julix <julixandrina@gmail.com>
2019-10-23 17:08:49 +0300

    Final refactoring

--- index.php
+++ index.php
@@ -11,36 +11,31 @@
     <script> </script>
 </head>
 <body>
-
-    <div class="page">
-        <div class="grid-container">
-            <div class="left"></div>
-            <div class="right"></div>
-
-
+    <div class="page-verify">
+        <div class="backdrop-grid-container">
+            <div class="left-part"></div>
+            <div class="right-part"></div>
         </div>
         <div class="container">
-            <div class="content">
-                <a class="to-back" href="#">
+            <div class="main-block">
+                <a class="back-arrow" href="#">
                     <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M23.5 9.5H1.5M1.5 9.5L9.5 1.5M1.5 9.5L9.5 18.5" stroke="black" stroke-width="2"/>
                     </svg>
                 </a>
-                <div class="main-content">
-                    <div class="title">Verify your email</div>
-                    <div class="to-action">You will need to verify your email to complete registration</div>
-                    <img src="/img/mailbox.svg" alt="">
-                    <div class="info">An email has been to zexjulizrizh544@gmail.com with a link to verify your account.
+                <div class="info-content">
+                    <div class="message main-font">Verify your email</div>
+                    <div class="follow-message main-font">You will need to verify your email to complete registration</div>
+                    <img src="/img/mailbox.svg" alt="picture post-box">
+                    <div class="instruction-to-confirm main-font">An email has been to follow-message@gmail.com with a link to verify your account.
                         If you have not received the email after a few minutes, please check your spam folder</div>
                     <div class="buttons">
-                        <button type="button" class="resend button">Resend Email</button>
-                        <a href="#" class="support button">Contact Support</a>
+                        <button type="button" class="resend-email button main-font">Resend Email</button>
+                        <a href="#" class="contact-support button main-font">Contact Support</a>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
-
 </body>
 </html>
